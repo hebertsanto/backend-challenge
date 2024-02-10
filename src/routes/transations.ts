e6026c5cd91318dbc9b2448fb6088cdc71a36592
hebertsanto <hebertsantos0704@gmail.com>
2024-02-10 15:09:59 -0300

    chore: change name routes

--- src/routes/transations.ts
+++ src/routes/transations.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import { listAllTransitionsController } from '../http/controllers/transitions/list-all-transitions-controller';
 import { createTransationController } from '../http/controllers/transitions/create-transation-controler';
 import { listTransitionByIdController } from '../http/controllers/transitions/list-transition-controller';
+import { generatePdfTransaction } from '../http/controllers/transitions/list-pdf-crontroller';
 
 export const transationsRoutes = Router();
 
-transationsRoutes.get('/transations', listAllTransitionsController);
-transationsRoutes.get('/transitions/:id', listTransitionByIdController );
-transationsRoutes.post('/transitions', createTransationController);
+transationsRoutes.get('/transaction/all/:id', listAllTransitionsController);
+transationsRoutes.get('/transaction/:id/file', generatePdfTransaction);
+transationsRoutes.get('/transaction/:id', listTransitionByIdController );
+transationsRoutes.post('/transaction', createTransationController);
