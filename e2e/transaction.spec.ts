aa71371f5eb37720467c3999c917ce9cda90c680
hebertsanto <hebertsantos0704@gmail.com>
2024-02-20 20:45:03 -0300

    test: add more one e2e test

--- e2e/transaction.spec.ts
+++ e2e/transaction.spec.ts
@@ -1,16 +1,17 @@
 import { AppRoutes } from '../src/http/express';
 import request from 'supertest';
 
-describe('teste', () => {
-  //eslint-disable-next-line
-  let app: any;
+//eslint-disable-next-line
+let app: any;
 
-  beforeAll(async () => {
-    await AppRoutes.initialize();
-    app = AppRoutes.getInstance();
-  });
+beforeAll(async () => {
+  await AppRoutes.initialize();
+  app = AppRoutes.getInstance();
+});
 
-  it('must return transactions from a card', async () => {
+describe('should return all transactions from card', () => {
+
+  it('/GET /transaction/all/card_id', async () => {
     const card_id = '36ffb3e9-e7ac-4323-8395-606c17205339';
     const response = await request(app)
       .get(`/transaction/all/${card_id}`);
@@ -18,4 +19,15 @@ describe('teste', () => {
     expect(response.status).toBe(200);
     expect(response.body.msg).toEqual('all transations are available');
   });
+
+});
+
+describe('should return NotFoundResourse if card_id does not exist', () => {
+  it('/GET /transaction/all/non_exist_card', async() => {
+    const response = await request(app)
+      .get('/transaction/all/non_existent_card');
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('error', 'NotFoundResource');
+  } );
+
 });
