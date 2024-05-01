df72e4343f861ab83e3a94a962224b66057b015c
hebertsanto <hebertsantos0704@gmail.com>
2024-05-01 20:19:43 -0300

    chore: add some middlewares to the project

--- src/http/express/index.ts
+++ src/http/express/index.ts
@@ -3,6 +3,10 @@ import { v1Router } from '../../routes';
 import { zodErrorMiddleware } from '../middlewares/zod-error';
 import { logsMiddleware } from '../middlewares/logs';
 import { logger } from '../../helpers/logger';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import compression from 'compression';
 
 export class AppRoutes {
   private expressApp: Express;
@@ -16,6 +20,12 @@ export class AppRoutes {
   private middlewares(): void {
     this.expressApp.use(zodErrorMiddleware);
     this.expressApp.use(logsMiddleware);
+
+    this.expressApp.use(compression());
+    this.expressApp.use(helmet());
+    this.expressApp.use(cors());
+    this.expressApp.use(bodyParser.json());
+    this.expressApp.use(bodyParser.urlencoded({ extended: true }));
   }
 
   private routes() {
